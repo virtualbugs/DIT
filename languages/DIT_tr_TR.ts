44e4d7769557d85da54f2dbf47bf3c75de608662
_caliskan_ <caliskan2mustafa@gmail.com>
2023-01-07 20:19:21 +0300

    turkish language support is added

---
+++ languages/DIT_tr_TR.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>DialogNewTask</name>
+    <message>
+        <location filename="../src/dialognewtask.ui" line="14"/>
+        <source>New Task</source>
+        <translation>Yeni Görev</translation>
+    </message>
+    <message>
+        <location filename="../src/dialognewtask.ui" line="22"/>
+        <source>Task</source>
+        <translation>Görev</translation>
+    </message>
+    <message>
+        <location filename="../src/dialognewtask.ui" line="29"/>
+        <source>Tag</source>
+        <translation>Etiket</translation>
+    </message>
+    <message>
+        <location filename="../src/dialognewtask.ui" line="42"/>
+        <source>Start Date</source>
+        <translation>Başlangıç</translation>
+    </message>
+    <message>
+        <location filename="../src/dialognewtask.ui" line="49"/>
+        <source>End Date</source>
+        <translation>Bitiş</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Ana Menu</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="52"/>
+        <source>ToDo</source>
+        <translation>Yapılacaklar</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="98"/>
+        <source>Add</source>
+        <translation>Ekle</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="114"/>
+        <source>Remove</source>
+        <translation>Sil</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="140"/>
+        <source>Done</source>
+        <translation>Tamamlananlar</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="10"/>
+        <source>Do It Today</source>
+        <translation>DIT (Bugünün İşini Yarına Bırakma)</translation>
+    </message>
+</context>
+</TS>
